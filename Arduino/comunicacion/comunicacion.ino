c89f95ede5ce6c7714b6ad46655050e22d88a19e
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-03-21 08:57:57 -0600

    actualizanco comunicacion

--- Arduino/comunicacion/comunicacion.ino
+++ Arduino/comunicacion/comunicacion.ino
@@ -1,8 +1,23 @@
 
 
-// ----------  Entradas salidas
-int DO_00 = 26;
+// ----------  Señales digitales
+#define numeroDeX 16
+byte X[numeroDeX];
 
+int DI_00 = 36;
+int DI_01 = 39;
+int DI_02 = 34;
+int DI_03 = 35;
+
+#define numeroDeY 16
+byte Y[numeroDeY];
+
+int DO_00 = 26;
+int DO_01 = 27;
+int DO_02 = 14;
+int DO_03 = 12;
+int DO_04 = 13;
+int DO_05 = 2;    // LED CONECTADO a la tarjeta
 
 // ----------  Temporizadores
 #define  numeroDeTON 16
@@ -21,6 +36,24 @@ struct temporizadorAux {
 void actualizarTON (int);
 
 // ----------  Comunicación
+/*
+    00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+   |CI| I  N V | TI  | NUM | LON | DATOS ...........        |V |CF| 
+    CI  - Caracter de inicio
+    INV - Para invernadero
+    TI  - Tipo de instrucción
+    NUM - Numero de instrucción
+    LON - Longitud
+    V   - Verificación
+    CF  - Caracter de final
+ */
+#define bufferIndiceMaximo  120
+byte bufferLectura[bufferIndiceMaximo];
+int bufferIndice = 0;
+
+void colocarDatosEnBufferSerial(void);
+void imprimirTrama(byte *, int, int);
+ 
 void leerInstruccion(int);
 
 
@@ -30,20 +63,58 @@ int contador;
 
 void setup() {
 
+  pinMode(DI_00, INPUT);
+  pinMode(DI_01, INPUT);
+  pinMode(DI_02, INPUT);
+  pinMode(DI_03, INPUT);
+      
   pinMode(DO_00, OUTPUT);
-  Serial.begin(9600);
-  TON[0].tiempo = (unsigned long)1 * 1000;      // 1 segundo
+  pinMode(DO_01, OUTPUT);
+  pinMode(DO_02, OUTPUT);
+  pinMode(DO_03, OUTPUT);
+  pinMode(DO_04, OUTPUT);
+  pinMode(DO_05, OUTPUT);
 
+  Serial.begin(9600);
+  
+  TON[0].tiempo = (unsigned long)1 * 1000;     // 1 segundo
+  TON[1].tiempo = (unsigned long)1 * 400;      // 0.4 segundos
+  TON[2].tiempo = (unsigned long)1 * 800;      // 0.8 segundos
+
+  
   contador = 0;
 }
 
 void loop() {
+  colocarDatosEnBufferSerial();
+  
   TON[0].entrada = !TON[0].salida;
   actualizarTON(0);
 
   if (TON[0].salida) {
     contador += 1;
   }
+
+  // Parpadeo indicando que el programa esta funcionando
+  TON[1].entrada = !TON[2].salida;
+  actualizarTON(1);
+
+  TON[2].entrada = TON[1].salida;
+  actualizarTON(2);
+
+  Y[5] = TON[1].salida & !TON[2].salida;
+
+  X[0] = digitalRead(DI_00);
+  X[1] = digitalRead(DI_01);
+  X[2] = digitalRead(DI_02);
+  X[3] = digitalRead(DI_03);
+
+  digitalWrite(DO_00, Y[0]);
+  digitalWrite(DO_01, Y[1]);
+  digitalWrite(DO_02, Y[2]);
+  digitalWrite(DO_03, Y[3]);
+  digitalWrite(DO_04, Y[4]);
+  digitalWrite(DO_05, Y[5]);
 
   //Serial.print("contador: ");
   //Serial.write(contador);
@@ -56,15 +127,42 @@ void loop() {
   //Serial.print(TON[0].salida);
   //Serial.print("\n");
 
-   while (Serial.available()) {
-    int caracter = Serial.read();
-    //Serial.write(caracter*10);
-    leerInstruccion(caracter);
-      
-    
-   }
+
 
 }
+// ----------  Comunicación
+
+void colocarDatosEnBufferSerial(){
+  byte caracter;
+  int aux = 0;
+  int indiceAuxiliar = 0;
+
+  while (Serial.available()) {
+    caracter = Serial.read();
+    
+    bufferLectura[bufferIndice++] = caracter;
+    
+
+    if (bufferIndice > bufferIndiceMaximo - 10) {
+      aux = bufferIndiceMaximo >> 1;
+      for (int i = aux; i < bufferIndiceMaximo; i++){
+        bufferIndice = i - aux;
+        bufferLectura[bufferIndice] = bufferLectura[i];
+      }
+    }
+
+    imprimirTrama(bufferLectura, 0, bufferIndice);
+    
+   }
+}
+void imprimirTrama(byte *ptrTrama, int inicio, int tamanio) {
+  Serial.print("\n>>");
+  for (int k = inicio; k < inicio + tamanio; k++) {
+    Serial.write(ptrTrama[k]);
+  }
+  
+}
+
 
 void leerInstruccion(int caracter) {
       switch (caracter) {
