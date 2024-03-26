8a625cf7b0902b51a8d367b403fbc1f32b67d476
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-03-26 15:29:49 -0600

    a

--- Python/Invernadero/Inicio.py
+++ Python/Invernadero/Inicio.py
@@ -14,6 +14,6 @@ class Inicio(Ventana):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Inicio()
-    window.show();
+    window.show()
     sys.exit(app.exec())
        
